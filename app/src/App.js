e948ee03c34bbf00f55d4c0b849a1267b2564597
Jason Clifton <j.clifton1473@gmail.com>
2017-09-14 23:13:58 -0600

    trying stuff

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import KioskView from './Components/KioskView';
 
 // https://github.com/electron/electron/issues/7300
 // We don't want to bundle electron in the webpack process so we use it's globally exposed require method.
